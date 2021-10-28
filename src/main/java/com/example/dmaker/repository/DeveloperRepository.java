41d64ad67b5ef1527494aec13f9889b9e95d551d
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-10-28 13:04:11 +0900

    [add&update] validation 및 errorCode 적용

--- src/main/java/com/example/dmaker/repository/DeveloperRepository.java
+++ src/main/java/com/example/dmaker/repository/DeveloperRepository.java
@@ -3,6 +3,9 @@ package com.example.dmaker.repository;
 import com.example.dmaker.entity.Developer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DeveloperRepository extends JpaRepository<Developer, Long> {
 
+    Optional<Developer> findByMemberId(String memberId);
 }
--- src/main/java/com/example/dmaker/service/DMakerService.java
+++ src/main/java/com/example/dmaker/service/DMakerService.java
@@ -2,6 +2,8 @@ package com.example.dmaker.service;
 
 import com.example.dmaker.dto.CreateDeveloper;
 import com.example.dmaker.entity.Developer;
+import com.example.dmaker.exception.DMakerErrorCode;
+import com.example.dmaker.exception.DMakerException;
 import com.example.dmaker.repository.DeveloperRepository;
 import com.example.dmaker.type.DeveloperLevel;
 import com.example.dmaker.type.DeveloperSkillType;
@@ -9,7 +11,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import javax.validation.Valid;
+
+import java.util.Optional;
+
+import static com.example.dmaker.exception.DMakerErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +24,9 @@ public class DMakerService {
 
     @Transactional      // AOP 공통모듈 => 어노테이션으로 , TranctionInterceptor 내 invocation : Aop joint 호출이었다!
     public void createDeveloper(CreateDeveloper.Request request) {
+
+        validateCreateDeveloperRequest(request);
+
         Developer developer = Developer.builder()
                 .developerLevel(DeveloperLevel.JUNGNIOR)
                 .developerSkillType(DeveloperSkillType.BACK_END)
@@ -28,5 +36,36 @@ public class DMakerService {
                 .build();
 
         developerRepository.save(developer);
+    }
+
+    private void validateCreateDeveloperRequest(CreateDeveloper.Request request) {
+
+        // business validation
+        var developerLevel = request.getDeveloperLevel();
+        var experienceYears = request.getExperienceYears();
+
+        if (developerLevel == DeveloperLevel.SENIOR
+                && experienceYears < 10) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+        if (developerLevel == DeveloperLevel.JUNIOR
+                && (experienceYears < 4 || experienceYears > 10)) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+        if (developerLevel == DeveloperLevel.JUNIOR
+                && experienceYears > 4) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+        var developer = developerRepository.findByMemberId(request.getMemberId());
+
+//        if (developer.isPresent()) {
+//            throw new DMakerException(DUPLICATED_MEMBER_ID);
+//        }
+        developer.ifPresent( d -> {
+            throw new DMakerException(DUPLICATED_MEMBER_ID);
+        });
     }
 }
