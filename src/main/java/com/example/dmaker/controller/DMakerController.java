95c90544402fc757fac13532a151309315cdfd19
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-10-28 12:39:15 +0900

    [add] CreateDeveloper.java, vaildation 적용

--- src/main/java/com/example/dmaker/controller/DMakerController.java
+++ src/main/java/com/example/dmaker/controller/DMakerController.java
@@ -1,12 +1,15 @@
 package com.example.dmaker.controller;
 
+import com.example.dmaker.dto.CreateDeveloper;
 import com.example.dmaker.service.DMakerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
@@ -31,11 +34,13 @@ public class DMakerController {
     }
 
     @PostMapping("/create-developer")
-    public List<String> createDevelopers() {
+    public List<String> createDevelopers(
+            @Valid @RequestBody CreateDeveloper.Request request
+            ) {
 
-        log.info("POST /create-developer HTTP/1.1");
+        log.info("request : {}", request);
 
-        dMakerService.createDeveloper();
+        dMakerService.createDeveloper(request);
 
         return Collections.singletonList("sg.do");
     }
--- src/main/java/com/example/dmaker/service/DMakerService.java
+++ src/main/java/com/example/dmaker/service/DMakerService.java
@@ -1,5 +1,6 @@
 package com.example.dmaker.service;
 
+import com.example.dmaker.dto.CreateDeveloper;
 import com.example.dmaker.entity.Developer;
 import com.example.dmaker.repository.DeveloperRepository;
 import com.example.dmaker.type.DeveloperLevel;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +18,7 @@ public class DMakerService {
     private final DeveloperRepository developerRepository;
 
     @Transactional      // AOP 공통모듈 => 어노테이션으로 , TranctionInterceptor 내 invocation : Aop joint 호출이었다!
-    public void createDeveloper() {
+    public void createDeveloper(CreateDeveloper.Request request) {
         Developer developer = Developer.builder()
                 .developerLevel(DeveloperLevel.JUNGNIOR)
                 .developerSkillType(DeveloperSkillType.BACK_END)
